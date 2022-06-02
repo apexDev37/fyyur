94731f94a386916746b2d2092dca533f2f35301b
apexdev <mwangi.em37@gmail.com>
2022-06-02 19:55:35 +0300

    Added Migration script: Alter Artist Table
    Modified corresponding Model Artist

--- app.py
+++ app.py
@@ -47,30 +47,64 @@ class Venue(db.Model):
     website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120), default='We are on the lookout for a local artist to play. Please call us.')
+    seeking_description = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
 
     # @hybrid_property
-    # def 
+    # def past_shows(self):
+    #   return (
+    #     Show.query
+    #       .filter(Show.venue_id == self.id)
+    #       .filter(Show.start_time < datetime.now())
+    #   )
+
+    # @hybrid_property
+    # def upcoming_shows(self):
+    #   return (
+    #     Show.query
+    #       .filter(Show.venue_id == self.id)
+    #       .filter(Show.start_time >= datetime.now())
+    #   )
 
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate [COMPLETED]
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    genres = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(320))
+    image_link = db.Column(db.String(500))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # @hybrid_property
+    # def past_shows(self):
+    #   return (
+    #     Show.query
+    #       .filter(Show.artist_id == self.id)
+    #       .filter(Show.start_time < datetime.now())
+    #   )
+
+    # @hybrid_property
+    # def upcoming_shows(self):
+    #   return (
+    #     Show.query
+    #       .filter(Show.artist_id == self.id)
+    #       .filter(Show.start_time >= datetime.now())
+    #   )
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate [COMPLETED]
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+
 
 #----------------------------------------------------------------------------#
 # Filters.
