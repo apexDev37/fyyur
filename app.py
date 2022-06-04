6862c92f48ceb1d77b57907ff12b524f410ce039
apexdev <mwangi.em37@gmail.com>
2022-06-04 11:27:16 +0300

    Completed TODOs: replace with real venue data from the venues table, using venue_id.
    Work modified in the app.py script, @app.route('/venues/<int:venue_id>'), def show_venue(venue_id).

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ class Venue(db.Model):
         Show.query
           .filter(Show.venue_id == self.id)
           .filter(Show.start_time < datetime.now())
+          .all()
       )
 
     @hybrid_property
@@ -71,6 +72,7 @@ class Venue(db.Model):
         Show.query
           .filter(Show.venue_id == self.id)
           .filter(Show.start_time >= datetime.now())
+          .all()
       )
 
 
@@ -101,6 +103,7 @@ class Artist(db.Model):
         Show.query
           .filter(Show.artist_id == self.id)
           .filter(Show.start_time < datetime.now())
+          .all()
       )
 
     @hybrid_property
@@ -109,6 +112,7 @@ class Artist(db.Model):
         Show.query
           .filter(Show.artist_id == self.id)
           .filter(Show.start_time >= datetime.now())
+          .all()
       )
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate [COMPLETED]
@@ -189,9 +193,21 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
+  # TODO: replace with real venue data from the venues table, using venue_id [COMPLETED]
 
-  venue = db.session.query(Venue).get(1)
+  venue = db.session.query(Venue).get(venue_id)
+  past_shows = venue.past_shows
+  past_shows_artist_data = [
+    {'artist_id': show.Artist.id, 'artist_name': show.Artist.name, 
+    'artist_image_link': show.Artist.image_link, 'start_time': format_datetime(show.start_time)} 
+    for show in past_shows
+  ]
+  upcoming_shows = venue.upcoming_shows
+  upcoming_shows_artist_data = [
+    {'artist_id': show.Artist.id, 'artist_name': show.Artist.name, 
+    'artist_image_link': show.Artist.image_link, 'start_time': format_datetime(show.start_time)} 
+    for show in upcoming_shows
+  ]
   data = {
     'id': venue.id,
     'name': venue.name,
@@ -205,71 +221,11 @@ def show_venue(venue_id):
     'seeking_talent': venue.seeking_talent,
     'seeking_description': venue.seeking_description,
     'image_link': venue.image_link,
-    'past_shows': [{
-      'artist_id': 4,
-      'artist_name': "Guns N Petals",
-      'artist_image_link': "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-      'start_time': "2019-05-21T21:30:00.000Z"
-    }],
-    'upcoming_shows': [],
-    'past_shows_count': 1,
-    'upcoming_shows_count': 0,
+    'past_shows': past_shows_artist_data,
+    'upcoming_shows': upcoming_shows_artist_data,
+    'past_shows_count': len(past_shows),
+    'upcoming_shows_count': len(upcoming_shows),
   }
-  # data2={
-  #   "id": 2,
-  #   "name": "The Dueling Pianos Bar",
-  #   "genres": ["Classical", "R&B", "Hip-Hop"],
-  #   "address": "335 Delancey Street",
-  #   "city": "New York",
-  #   "state": "NY",
-  #   "phone": "914-003-1132",
-  #   "website": "https://www.theduelingpianos.com",
-  #   "facebook_link": "https://www.facebook.com/theduelingpianos",
-  #   "seeking_talent": False,
-  #   "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-  #   "past_shows": [],
-  #   "upcoming_shows": [],
-  #   "past_shows_count": 0,
-  #   "upcoming_shows_count": 0,
-  # }
-  # data3={
-  #   "id": 3,
-  #   "name": "Park Square Live Music & Coffee",
-  #   "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-  #   "address": "34 Whiskey Moore Ave",
-  #   "city": "San Francisco",
-  #   "state": "CA",
-  #   "phone": "415-000-1234",
-  #   "website": "https://www.parksquarelivemusicandcoffee.com",
-  #   "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-  #   "seeking_talent": False,
-  #   "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-  #   "past_shows": [{
-  #     "artist_id": 5,
-  #     "artist_name": "Matt Quevedo",
-  #     "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-  #     "start_time": "2019-06-15T23:00:00.000Z"
-  #   }],
-  #   "upcoming_shows": [{
-  #     "artist_id": 6,
-  #     "artist_name": "The Wild Sax Band",
-  #     "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #     "start_time": "2035-04-01T20:00:00.000Z"
-  #   }, {
-  #     "artist_id": 6,
-  #     "artist_name": "The Wild Sax Band",
-  #     "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #     "start_time": "2035-04-08T20:00:00.000Z"
-  #   }, {
-  #     "artist_id": 6,
-  #     "artist_name": "The Wild Sax Band",
-  #     "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-  #     "start_time": "2035-04-15T20:00:00.000Z"
-  #   }],
-  #   "past_shows_count": 1,
-  #   "upcoming_shows_count": 1,
-  # }
-  # data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
   return render_template('pages/show_venue.html', venue=data)
 
 #  Create Venue
